f0d156f6bddbc2395b0b235407e54771f4a7202e
Eric Xiao <exiao3@gmail.com>
2013-08-08 14:22:15 -0700

    Django app setup

--- kudos/kudosapp/views.py
+++ kudos/kudosapp/views.py
@@ -1,1 +1,8 @@
 # Create your views here.
+
+from django.template import RequestContext
+from django.shortcuts import render_to_response, redirect, get_object_or_404
+
+
+def home(request):
+    return render_to_response('home.html', context_instance=RequestContext(request))
