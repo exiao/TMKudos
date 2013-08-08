a073b5f64d29854020343fd08d4d07c34f1e95b0
Eric Xiao <exiao3@gmail.com>
2013-08-08 15:48:34 -0700

    added flagged

--- kudos/kudosapp/models.py
+++ kudos/kudosapp/models.py
@@ -45,6 +45,7 @@ class Employee(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
+    flagged = models.BooleanField(default=False)
 
 
 class Kudos(models.Model):
