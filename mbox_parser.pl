1172462aa2f9578f3612db113aa69bf8c098a834
pgrandin <grandinp@altern.org>
2013-08-09 18:16:33 +0000

    Added body filtering

--- mbox_parser.pl
+++ mbox_parser.pl
@@ -2,6 +2,7 @@ use Config::Simple;
 use Data::Dumper;
 use Net::IMAP::Simple::SSL;
 use Email::Simple;
+use Email::Send;
 use Mail::Address;
 use DBI;
 use HTML::Entities;
@@ -68,19 +69,29 @@ $imap->login($user => $pass) || die "Unable to login $user/$pass\n";
 				$from_id=get_employee_id($dbh,$1);
 			}
 		
+		
 			my @message_lines = $imap->get( $i ) or die $imap->errstr;
 			my $hashtags="";
 			foreach $line (@message_lines) {
 				if ($line=~/\+kudo/i && $line !~/=/ ) {
-					print "Found hashtag line : [$line}\n";
+					# print "Found hashtag line : [$line}\n";
 					while($line =~/#(\w+)/g){
-						print "Found Hashtag $1\n";
+						# print "Found Hashtag $1\n";
 						$hashtags.="$1,";
 					}
 				}
 			}
 		
 			my @addrs = Mail::Address->parse($es->header('To'));
+			
+			my $message = $imap->get( $i ) or die $imap->errstr;
+			
+			if( $message=~/Content-Type: text\/plain.+?\n(.+)Content-Type: text\/html/s ) {
+				$filtered_body=$1;
+			}
+			
+			print "Found ".scalar(@addrs)." to: \n";
+			
 		 	foreach $addr (@addrs) {
 		     	print " --> ".$addr->format,"\n";
 		    	 if( $addr->format=~/([a-z\.]+)\@tubemogul.com/ ){
@@ -88,7 +99,7 @@ $imap->login($user => $pass) || die "Unable to login $user/$pass\n";
 					if(is_kudo_unique($dbh,$es->header('Message-ID'),$to_id) && $to_id != $from_id){
 			  			$dbh->do("insert into kudosapp_kudos 
 							(from_employee_id, to_employee_id, created, subject, body, tags, message_id) values 
-							($from_id, $to_id, NOW(), '".$es->header('Subject')."', '<>', '$hashtags', '".$es->header('Message-ID')."')");
+							($from_id, $to_id, NOW(), '".$es->header('Subject')."', '$filtered_body', '$hashtags', '".$es->header('Message-ID')."')");
 					}
 	     		} else {
 					die "Failed to parse :(\n";
@@ -96,7 +107,42 @@ $imap->login($user => $pass) || die "Unable to login $user/$pass\n";
 		 	}
 		} else { 
 			print "Skipped, not the first mail of the thread\n";
+			
+			$body="
+Dear Moguler,
+
+You seemed to have kept kudos@tubemogul.com cc'd to this email thread. Please note that any replies to this thread will not register as any new kudos.
+
+Thank you,
+The Kudo Team";
+
+			my $mailer = Email::Send->new( {
+			    mailer => 'SMTP::TLS',
+			    mailer_args => [
+			        Host => 'smtp.gmail.com',
+			        Port => 587,
+			        User => $user,
+			        Password => $pass,
+			    ]
+			} );
+			
+			use Email::Simple::Creator; # or other Email::
+			my $email = Email::Simple->create(
+			    header => [
+			        From    => $user,
+			        To      => $es->header('From'),
+			        Subject => 'Kudos : Woops!',
+			    ],
+			    body => $body,
+			);
+			
+			eval { $mailer->send($email) };	
+			die "Error sending email: $@" if $@;
+			print "Mail sent to ".$es->header('From')."\n";		
+			
 		}
 
 }
+
+system("/usr/bin/python /var/www/TMKudos/kudos/manage.py update_index");
 $imap->quit;
