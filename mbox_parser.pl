152569b80adb3f48aa77ec5a4eb0a791f22fc9d1
pgrandin <grandinp@altern.org>
2013-08-09 16:23:36 +0000

    Added anticheat

--- mbox_parser.pl
+++ mbox_parser.pl
@@ -24,7 +24,7 @@ $dbh = DBI->connect($dsn, $db_user, $db_pass);
 my $imap = Net::IMAP::Simple::SSL->new($host) || die "Unable to connect to IMAP: $Net::IMAP::Simple::errstr\n";
  
 
-sub get_employee_id {
+sub get_employee_id ($$) {
 	my ($dbh,$e_name)=@_;
 	my $sth = $dbh->prepare("SELECT id,email FROM kudosapp_employee where email='$e_name\@tubemogul.com'");
 	$sth->execute();
@@ -36,7 +36,17 @@ sub get_employee_id {
 	return $to_id;
 }
  
-$imap->login($user => $pass) || die "Unable to logini $user/$pass\n";
+sub is_kudo_unique ($$$){
+	my ($dbh,$message_id,$to)=@_;
+	my $sth = $dbh->prepare("SELECT id,message_id FROM kudosapp_kudos where message_id='$message_id' and to_employee_id='$to'");
+	$sth->execute();
+	$count=$sth->rows;
+	$sth->finish();
+	return $count==0;
+}
+  
+ 
+$imap->login($user => $pass) || die "Unable to login $user/$pass\n";
   
     my $nm = $imap->select('INBOX');
 
@@ -50,39 +60,43 @@ $imap->login($user => $pass) || die "Unable to logini $user/$pass\n";
         my $es = Email::Simple->new(join '', @{ $imap->top($i) } );
 
         printf("[%03d] %s\n", $i, $es->header('Subject'));
-	if($es->header('In-Reply-To') eq ""){
-		
-		print  $es->header('From')."\n";
-	    	if( $es->header('From')=~/([a-z\.]+)\@tubemogul.com/ ){
-			$from_id=get_employee_id($dbh,$1);
-		}
 	
-		my @message_lines = $imap->get( $i ) or die $imap->errstr;
-		my $hashtags="";
-		foreach $line (@message_lines) {
-			if ($line=~/\+kudo/i && $line !~/=/ ) {
-				print "Found hashtag line : [$line}\n";
-				while($line =~/#(\w+)/g){
-					print "Found Hashtag $1\n";
-					$hashtags.="$1,";
+		if($es->header('In-Reply-To') eq ""){
+		
+			print  $es->header('From')."\n";
+		    	if( $es->header('From')=~/([a-z\.]+)\@tubemogul.com/ ){
+				$from_id=get_employee_id($dbh,$1);
+			}
+		
+			my @message_lines = $imap->get( $i ) or die $imap->errstr;
+			my $hashtags="";
+			foreach $line (@message_lines) {
+				if ($line=~/\+kudo/i && $line !~/=/ ) {
+					print "Found hashtag line : [$line}\n";
+					while($line =~/#(\w+)/g){
+						print "Found Hashtag $1\n";
+						$hashtags.="$1,";
+					}
 				}
 			}
-		}
-	
-		my @addrs = Mail::Address->parse($es->header('To'));
-	 	foreach $addr (@addrs) {
-	     		print " --> ".$addr->format,"\n";
-	    	 	if( $addr->format=~/([a-z\.]+)\@tubemogul.com/ ){
-				$to_id=get_employee_id($dbh,$1);
-		  		$dbh->do("insert into kudosapp_kudos (from_employee_id, to_employee_id, created, subject, body, tags) values ($from_id, $to_id, NOW(), '".$es->header('Subject')."', '<>', '$hashtags')");
+		
+			my @addrs = Mail::Address->parse($es->header('To'));
+		 	foreach $addr (@addrs) {
+		     	print " --> ".$addr->format,"\n";
+		    	 if( $addr->format=~/([a-z\.]+)\@tubemogul.com/ ){
+					$to_id=get_employee_id($dbh,$1);
+					if(is_kudo_unique($dbh,$es->header('Message-ID'),$to_id) && $to_id != $from_id){
+			  			$dbh->do("insert into kudosapp_kudos 
+							(from_employee_id, to_employee_id, created, subject, body, tags, message_id) values 
+							($from_id, $to_id, NOW(), '".$es->header('Subject')."', '<>', '$hashtags', '".$es->header('Message-ID')."')");
+					}
 	     		} else {
-				die "Failed to parse :(\n";
+					die "Failed to parse :(\n";
 	     		}
-	
-	 	}
-	} else { 
-		print "Skipped, not the first mail of the thread\n";
-	}
+		 	}
+		} else { 
+			print "Skipped, not the first mail of the thread\n";
+		}
 
 }
 $imap->quit;
